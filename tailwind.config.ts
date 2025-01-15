299c93fd7daf3cefa323ab854842963c1da8eb12
kylezap <kzapcic+github@gmail.com>
2025-01-14 21:21:16 -0500

    chore: add dependencies and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#121212",
+        foreground: "#f8f8f2",
+      },
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(100%)', opacity: 0 },
+          '50%': { opacity: 1 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 1s ease-in-out',
       },
     },
   },
