cf8de79d8d1200ead5c355a8d5b662ad1a8e3b6f
kylezap <kzapcic+github@gmail.com>
2026-02-05 10:18:31 -0500

    feat: add devgrid work

--- app/components/JobsList.tsx
+++ app/components/JobsList.tsx
@@ -2,15 +2,20 @@ export default function JobsList() {
   return (
     <section
       id="experience"
-      className="mx-auto max-w-7xl px-6 lg:px-8 pt-8 sm:p-8"
+      className="mx-auto max-w-7xl px-6 lg:px-8 pt-4 sm:pt-6"
     >
       
       <div className="mt-6 border-t border-white/10">
         <dl className="divide-y divide-white/10">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-white">Real Estate Broker</dt>
+        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm/6 font-medium text-white">Solutions Engineer</dt>
             <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
-              Redfin | <em>May 2015 - Present</em>
+              DevGrid | <em>Sep 2025 - Present</em>
+            </dd>
+          </div><div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm/6 font-medium text-white">Customer Success (Agent)</dt>
+            <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
+              Redfin | <em>May 2015 - Sep 2025</em>
             </dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -36,7 +41,7 @@ export default function JobsList() {
             </dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm/6 font-medium text-white">Business Development Representative</dt>
+            <dt className="text-sm/6 font-medium text-white">Business Development</dt>
             <dd className="mt-1 text-sm/6 text-gray-400 sm:col-span-2 sm:mt-0">
               Percussion | <em>June 2010 - May 2011</em>
             </dd>
